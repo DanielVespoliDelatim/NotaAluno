45c35e8297803a4c4e685dc77125554220e8e195
soudix <revengesvd@gmail.com>
2021-09-07 15:14:19 -0300

    2

---
+++ Aula4Ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Aula4Ex6 {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            
+           
+            Console.WriteLine("NOTA FINAL = " +aluno.NotaFinal().ToString("F2"),CultureInfo.InvariantCulture);
+
+            if (aluno.Aprovado()) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "+aluno.NotaRestante().ToString("F2",CultureInfo.InvariantCulture)+" PONTOS");
+            }
+
+
+
+        }
+    }
+}
